dfa4b7101c6501e2774c9a837575a98b342ac824
Koki4515 <payeer.1100@gmail.com>
2025-12-27 19:14:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`[HTTP] ${req.method} ${req.url}`);
+  next();
+});
+
 const db = new Database("fires.db");
 
 db.prepare(`
