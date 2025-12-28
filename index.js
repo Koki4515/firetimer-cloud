6b83f14570615ce1abab1eb017fe54d1ff512fb7
Koki4515 <payeer.1100@gmail.com>
2025-12-29 01:58:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// Используем bodyParser для парсинга JSON в теле запроса
 app.use(bodyParser.json());  // Для парсинга JSON в теле запроса
+
+// Путь к файлу FireTimerCloud.ini
+const FILE_PATH = path.join(__dirname, 'FireTimerCloud.ini');
 
 // Обработчик для загрузки данных в файл
 app.post('/upload_ini', (req, res) => {
     const data = req.body;
-    console.log('Полученные данные:', data);  // Логируем полученные данные
 
-    const filePath = path.join(__dirname, 'FireTimerCloud.ini');
-    
-    fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8', (err) => {
+    // Логируем полученные данные
+    console.log('Полученные данные:', data);
+
+    // Сохраняем данные в INI файл
+    fs.writeFile(FILE_PATH, JSON.stringify(data, null, 2), 'utf8', (err) => {
         if (err) {
             console.error('Ошибка записи в файл:', err);
             return res.status(500).send('Ошибка записи в файл');
@@ -28,14 +33,14 @@ app.post('/upload_ini', (req, res) => {
 
 // Обработчик для получения данных из файла (GET запрос)
 app.get('/download_ini', (req, res) => {
-    const filePath = path.join(__dirname, 'FireTimerCloud.ini');
-
-    fs.readFile(filePath, 'utf8', (err, data) => {
+    // Читаем файл FireTimerCloud.ini
+    fs.readFile(FILE_PATH, 'utf8', (err, data) => {
         if (err) {
             console.error('Ошибка чтения файла:', err);
             return res.status(500).send('Ошибка чтения файла');
         }
 
+        // Отправляем содержимое файла как JSON
         res.json(JSON.parse(data));
     });
 });
