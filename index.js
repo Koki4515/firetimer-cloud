58b5c5ed84274b6eb30ba8d84ba13fb910615519
Koki4515 <payeer.1100@gmail.com>
2025-12-28 21:17:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,42 +4,42 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
-// Папка FireTimer рядом с FireTimer.lua
-const fireTimerDir = path.resolve(__dirname, 'FireTimer'); // Папка FireTimer будет в корне проекта
-const cloudIniFilePath = path.join(fireTimerDir, 'FireTimerCloud.ini'); // Путь к файлу INI в папке FireTimer
 
-// Проверка и создание папки FireTimer, если её нет
+const fireTimerDir = path.resolve(__dirname, 'FireTimer'); 
+const cloudIniFilePath = path.join(fireTimerDir, 'FireTimerCloud.ini'); 
+
+
 if (!fs.existsSync(fireTimerDir)) {
-    fs.mkdirSync(fireTimerDir);  // Если папки нет — создаём её
+    fs.mkdirSync(fireTimerDir); 
 }
 
-// Настройка Express для работы с JSON
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Запуск сервера
+
 app.listen(10000, () => {
     console.log('Server is running on port 10000');
 });
 
-// Обработка POST запроса на загрузку INI
+
 app.post('/upload_ini', (req, res) => {
     const fireData = req.body;
     console.log('Received POST data:', fireData);
 
-    // Проверка на наличие данных
+  
     if (!fireData.lastNormal || !fireData.nextNormal || !fireData.lastLvl3 || !fireData.nextLvl3) {
         return res.status(400).send('Invalid data: Missing required fields');
     }
 
-    // Формирование содержимого INI файла
+
     const iniContent = `[FireData]
 lastNormal=${fireData.lastNormal}
 nextNormal=${fireData.nextNormal}
 lastLvl3=${fireData.lastLvl3}
 nextLvl3=${fireData.nextLvl3}`;
 
-    // Запись данных в INI файл
+  
     fs.writeFile(cloudIniFilePath, iniContent, (err) => {
         if (err) {
             console.error('Error saving data to INI:', err);
@@ -49,14 +49,14 @@ nextLvl3=${fireData.nextLvl3}`;
     });
 });
 
-// Обработка запроса на скачивание INI
+
 app.get('/download_ini', (req, res) => {
     fs.readFile(cloudIniFilePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading INI file:', err);
             return res.status(500).send('Error reading INI file');
         }
-        res.send(data);  // Отправляем содержимое INI файла обратно клиенту
+        res.send(data);  
     });
 });
 
