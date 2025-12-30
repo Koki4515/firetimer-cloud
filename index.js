306a0058815b389d8691274079e64fd4d66a14f0
Koki4515 <payeer.1100@gmail.com>
2025-12-30 21:19:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 10000;
 app.use(bodyParser.json());
 app.use(cors());  
 
-const SERVER_FILE_PATH = path.join(__dirname, 'timer.ini');  // Путь к файлу timer.ini на сервере
-const CLOUD_FILE_PATH = path.join(__dirname, 'FireTimerCloud.ini');  // Локальная версия данных для клиента
+// Путь к файлу на сервере
+const SERVER_FILE_PATH = path.join(__dirname, 'timer.ini');  // Храним данные о пожарах
 
 // Функция для сохранения данных в файл
 function saveDataToServerFile(data) {
@@ -59,15 +59,7 @@ app.post('/upload_ini', (req, res) => {
     // Сохраняем данные в timer.ini
     saveDataToServerFile(data);
 
-    // Также сохраняем данные в FireTimerCloud.ini для клиента
-    fs.writeFile(CLOUD_FILE_PATH, JSON.stringify(data, null, 2), 'utf8', (err) => {
-        if (err) {
-            console.error('Ошибка записи в файл FireTimerCloud.ini на сервере:', err);
-            return res.status(500).send('Ошибка записи в файл FireTimerCloud.ini');
-        }
-        console.log('Данные успешно сохранены в FireTimerCloud.ini на сервере');
-        res.status(200).send('Данные успешно загружены в серверный файл timer.ini и FireTimerCloud.ini');
-    });
+    res.status(200).send('Данные успешно загружены в серверный файл timer.ini');
 });
 
 app.listen(PORT, () => {
